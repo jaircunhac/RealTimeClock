d23921b0069d3ccd6f435a81fd9b6d7b3dc0fb90
Jair Cunha <Jair@pbsenac.intra>
2024-08-30 09:54:07 -0300

    Finishing the project, JavaScript is done and some things were fixed in the HTML and CSS

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-let hour;
-let minute;
-let second;
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time(){
+    let auxTime = new Date();
+    
+    let hr = auxTime.getHours();
+    let min = auxTime.getMinutes();
+    let s = auxTime.getSeconds();
+
+    if(hr < 10){
+        hr = '0' + hr;
+    }
+
+    if(min < 10){
+        min = '0' + min;
+    }
+
+    if(s < 10){
+        s = '0' + s;
+    }
+
+    hour.textContent = hr;
+    minute.textContent = min;
+    seconds.textContent = s;
+})
